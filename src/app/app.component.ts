3c1c622a5c687c1d266eda3b6923c16de1541e70
unknown <camizupa.absti@gmail.com>
2023-08-23 03:03:13 -0300

    refresh token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spoti-app';
+  public data:any;
+  constructor(
+    private _service :SpotifyService
+  ){}
+  ngOnInit(){
+    this.data =this._service.getReleases();
+    if(this.data){
+      console.log(this.data)
+    }
+  }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,25 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders} from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor(private http: HttpClient) { 
+  private tokenUrl = 'https://accounts.spotify.com/api/token';
+  private clientId = 'd6b55734aa3f4b49af89ae8023b6c69e';
+  private clientSecret = '4659ca881ead4eed81c45197f071db34';
+  
+  constructor(private http: HttpClient) {}
+
+  getToken(): Observable<any> {
+    const body = new HttpParams()
+      .set('client_id', this.clientId)
+      .set('client_secret', this.clientSecret)
+      .set('grant_type', 'client_credentials');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post(this.tokenUrl, body.toString(), { headers });
   }
 
-  getReleases(){
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCtQMxl9eCMoMHiwy9x_RN3lkcAGgjLvWUWZtzNpHbQUmmJat9dzr0D-eAI-wtmOUzo-lfYekYCEkYALE1oMHb1hMApEcacm3XTXYIesZIM5gwwqwk'
-    });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers})
+  getReleases(): Observable<any> {
+    return this.getToken().pipe(
+      concatMap(response => {
+        const token = response.access_token;
+        const type = response.token_type;
+        const apiHeaders = new HttpHeaders().set('Authorization', `${type} ${token}`);
+        return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers: apiHeaders });
+      })
+    );
   }
 
-  getArtist(termino:string){
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCtQMxl9eCMoMHiwy9x_RN3lkcAGgjLvWUWZtzNpHbQUmmJat9dzr0D-eAI-wtmOUzo-lfYekYCEkYALE1oMHb1hMApEcacm3XTXYIesZIM5gwwqwk'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers})
+  getArtist(termino: string): Observable<any> {
+    return this.getToken().pipe(
+      concatMap(response => {
+        const token = response.access_token;
+        const type = response.token_type;
+        const apiHeaders = new HttpHeaders().set('Authorization', `${type} ${token}`);
+        return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers: apiHeaders });
+      })
+    );
   }
 }
